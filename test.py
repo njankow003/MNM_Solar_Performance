994f6e6ee0587e55ea795dfc3671e34b797278f0
njankowski <njankowski.uiowa.edu>
2023-09-23 13:16:03 -0500

    Beginning of Data Plotting

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 #Get the Pi Web API URL
-url = "https://itsnt2259.iowa.uiowa.edu/piwebapi/streams/F1AbEAVYciAZHVU6DzQbJjxTxWwimrOBShT7hGiW-T9RdLVfg_m58A6BxNVULugR7j2EabASVRTTlQyMjU5XFJZQU4gU0FOREJPWFxTT0xBUiBQUk9EVUNUSU9OXEJVUyBCQVJOfEZMT1cgVEFH/interpolated"
+url = "https://itsnt2259.iowa.uiowa.edu/piwebapi/streams/F1AbEAVYciAZHVU6DzQbJjxTxWwimrOBShT7hGiW-T9RdLVfg_m58A6BxNVULugR7j2EabASVRTTlQyMjU5XFJZQU4gU0FOREJPWFxTT0xBUiBQUk9EVUNUSU9OXEJVUyBCQVJOfEZMT1cgVEFH/recorded"
 
 #Make a GET request to the Pi Web API
 response = requests.get(url, auth=('njankowski', 'eje3+ydIjO9?-39'))
@@ -15,7 +15,7 @@ else:
     print("The request failed")
 
 #Get the Pi Web API response
-#response_json = response.json()
+response_json = response.json()
 
 #Print the Pi Web API response
-#print(response_json)
+print(response_json)
